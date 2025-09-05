955c2d8ca1f26555cde38364258ecccab9e7dfd9
akyoshid <akyoshid@student.42tokyo.jp>
2025-09-05 15:17:27 +0000

    [DONE] ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,11 +6,25 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 12:59:39 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/09/05 13:00:02 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/09/05 14:17:27 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EX01_ITER_HPP_
 # define EX01_ITER_HPP_
+
+#include <cstddef>
+
+template <typename T, typename F>
+void iter(T* array, size_t len, F func) {
+    for (size_t i = 0; i < len; ++i)
+        func(array[i]);
+}
+
+template <typename T, typename F>
+void iter(const T* array, size_t len, F func) {
+    for (size_t i = 0; i < len; ++i)
+        func(array[i]);
+}
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,12 +6,47 @@
 /*   By: akyoshid <akyoshid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 12:58:58 by akyoshid          #+#    #+#             */
-/*   Updated: 2025/09/05 13:00:32 by akyoshid         ###   ########.fr       */
+/*   Updated: 2025/09/05 15:17:01 by akyoshid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "iter.hpp"
 
+void increment_int(int& i) {
+    ++i;
+}
+
+void print_int(const int& i) {
+    std::cout << i;
+}
+
+void increment_char(char& c) {
+    ++c;
+}
+
+void print_char(const char& c) {
+    std::cout << c;
+}
+
 int main() {
+    int array[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+    iter(array, 8, print_int);
+    std::cout << std::endl;
+    iter(array, 8, increment_int);
+    iter(array, 8, print_int);
+    std::cout << std::endl;
+    int const const_array[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+    iter(const_array, 8, print_int);
+    std::cout << std::endl;
+    char str[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    iter(str, 8, print_char);
+    std::cout << std::endl;
+    iter(str, 8, increment_char);
+    iter(str, 8, print_char);
+    std::cout << std::endl;
+    const char const_str[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    iter(const_str, 8, print_char);
+    std::cout << std::endl;
     return 0;
 }
